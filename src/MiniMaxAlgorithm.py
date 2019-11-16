2c31b3257a77d0f59d2b5d9289786699417a530e
Lorenz Gerber <lorenz.gerber@lablicate.com>
2019-11-16 14:49:09 +0100

    modify time control

--- src/MiniMaxAlgorithm.py
+++ src/MiniMaxAlgorithm.py
@@ -9,6 +9,8 @@ class MiniMaxAlgorithm(OthelloAlgorithm):
         self.depth = 0
         self.tree = []
         self.depth_step = 1
+        self.time_limit = 10
+        self.move_order = {}
 
 
 
@@ -20,9 +22,9 @@ class MiniMaxAlgorithm(OthelloAlgorithm):
         self.start_position = othello_position
         start_time = time()
         while True:
-            heuristic = self.max_value( self.start_position, self.depth, -inf, +inf)
+            heuristic = self.max_value( self.start_position, self.depth, -inf, +inf, start_time)
             print(self.depth)
-            if (time() - start_time >= 2):
+            if (time() - start_time >= self.time_limit):
                  break
             
             self.depth = self.depth + self.depth_step
@@ -35,7 +37,7 @@ class MiniMaxAlgorithm(OthelloAlgorithm):
         self.depth = depth
 
 
-    def max_value(self, othello_position, depth, alpha, beta):
+    def max_value(self, othello_position, depth, alpha, beta, start_time):
         
         if (othello_position.check_is_leaf() or depth == 0):
             return (self.evaluator.evaluate(othello_position))
@@ -43,9 +45,13 @@ class MiniMaxAlgorithm(OthelloAlgorithm):
         value = -inf
         
         for child_move in othello_position.get_moves():
+
+            if (time() - start_time >= self.time_limit):
+                 break
+
             new_position = othello_position.clone()
             new_position.make_move(child_move)
-            value = max(value, self.min_value(new_position, depth, alpha, beta))
+            value = max(value, self.min_value(new_position, depth, alpha, beta, start_time))
             alpha = max(alpha, value)
             if alpha >= beta:
                 break
@@ -53,7 +59,7 @@ class MiniMaxAlgorithm(OthelloAlgorithm):
         return ( value )
 
 
-    def min_value(self, othello_position, depth, alpha, beta):
+    def min_value(self, othello_position, depth, alpha, beta, start_time):
 
         depth = depth - 1
         
@@ -63,9 +69,13 @@ class MiniMaxAlgorithm(OthelloAlgorithm):
         value = +inf
         
         for child_move in othello_position.get_moves():
+
+            if (time() - start_time >= self.time_limit):
+                 break
+
             new_position = othello_position.clone()
             new_position.make_move(child_move)
-            value = min(value, self.max_value(new_position, depth, alpha, beta))
+            value = min(value, self.max_value(new_position, depth, alpha, beta, start_time))
             alpha = min(alpha, value)
             if alpha >= beta:
                 break
