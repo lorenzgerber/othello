d14473bd7eb5efce6790777b66aad23b09496dc3
Lorenz Gerber <lorenz.gerber@provement.se>
2019-11-23 18:31:24 +0100

    test

--- src/HeuristicsEvaluator.py
+++ src/HeuristicsEvaluator.py
@@ -8,7 +8,7 @@ class HeuristicsEvaluator(OthelloEvaluator):
         self.value_edge = 10
         self.value_one_off = -100
         self.value_stone = 1
-        self.value_move = 50
+        self.value_move = 2
 
 
     def evaluate(self, othello_position):
