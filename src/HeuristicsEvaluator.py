281f184e0ca7b38dc43f951eb47e543ce953db4a
Lorenz Gerber <lorenz.gerber@lablicate.com>
2020-06-02 21:28:42 +0200

    all heuristics

--- src/HeuristicsEvaluator.py
+++ src/HeuristicsEvaluator.py
@@ -4,11 +4,11 @@ class HeuristicsEvaluator(OthelloEvaluator):
 
 
     def __init__(self):
-        self.value_corner = 10
-        self.value_edge = 5
-        self.value_one_off = -1
-        self.value_stone = 1
-        self.value_move = 1
+        self.value_corner = 20
+        self.value_edge = 10
+        self.value_one_off = -10
+        self.value_stone = 10
+        self.value_move = 5
 
 
     def evaluate(self, othello_position):
@@ -25,14 +25,14 @@ class HeuristicsEvaluator(OthelloEvaluator):
         score -= self.__check_edges('B')
 
         # Check one square off edges
-        #score += self.__check_one_offs('W')
-        #score -= self.__check_one_offs('B')
+        score += self.__check_one_offs('W')
+        score -= self.__check_one_offs('B')
 
         # Number of
         score += self.count_stones(self.position)
 
         # mobility
-        #score += self.count_moves(self.position)
+        score += self.count_moves(self.position)
 
         return (score)
 
